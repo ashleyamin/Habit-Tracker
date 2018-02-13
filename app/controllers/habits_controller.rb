5dcf66f424602da9c1b5c38288e6bb167296f1f5
Ashley Amin <ashley.starks@gmail.com>
2018-02-12 19:07:23 -0500

    completed CRUD functionality

---
+++ app/controllers/habits_controller.rb
@@ -0,0 +1,47 @@
+class HabitsController < ApplicationController
+    def index
+      @habits = Habit.all
+    end
+
+    def show
+      @habit = Habit.find(params[:id])
+    end
+
+    def new
+      @habit = Habit.new
+    end
+
+    def update
+      @habit = Habit.find(params[:id])
+        if @habit.update_attributes(habit_params)
+          redirect_to habits_path
+        else
+          render :edit
+        end
+    end
+
+    def edit
+      @habit = Habit.find(params[:id])
+    end
+
+    def create
+      @habit = Habit.new(habit_params)
+      @types = Type.all
+        if @habit.save
+          redirect_to habit_path(@habit)
+        else
+          raise "Error"
+        end
+    end
+
+    def destroy
+      @habit = Habit.find(params[:id])
+      @habit.destroy
+      redirect_to habits_path
+    end
+
+    private
+    def habit_params
+      params.require(:habit).permit(:type_id, :description, :weekly_goal, :reminder)
+    end
+end
--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -2,4 +2,8 @@ class TypesController < ApplicationController
   def index
     @types = Type.all
   end
+
+  def show
+    @types = Type.find(params[:id])
+  end
 end
